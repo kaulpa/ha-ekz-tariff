188c701376867ebe59394aca327de2b4c791d3b2
cnc-lasercraft <96776729+cnc-lasercraft@users.noreply.github.com>
2026-03-09 06:19:57 +0100

    Update coordinator.py

--- custom_components/ekz_tariff/coordinator.py
+++ custom_components/ekz_tariff/coordinator.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import logging
 from dataclasses import dataclass
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from typing import Any
 
 from homeassistant.core import HomeAssistant
